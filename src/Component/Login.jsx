ca667900118e1768cfdf58fab8209cd1ae2a3b06
gitspecific707 <gitspecific707@gmail.com>
2025-05-30 15:05:46 +0530

    just commit

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  console.log("hello git")
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const navigate=useNavigate();
